0f100b5e7a49ac6594f99cd70f0dcb41fb47e12b
Jonathan Philips <johnphilips.995@gmail.com>
2021-05-16 19:05:15 +0400

    Create C-to-F-Temperature-Converter.py

---
+++ C-to-F-Temperature-Converter.py
@@ -0,0 +1,3 @@
+temperature = int(input("Enter temperature in Celsius: "))
+temperature_f = (temperature * 1.8) + 32
+print("The temperature in Fahrenheit is ", temperature_f," degrees Fahrenheit.")
