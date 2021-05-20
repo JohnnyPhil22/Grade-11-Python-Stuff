a4e84bba9fea98401505136cc02d2e452b04df9f
Jonathan Philips <johnphilips.995@gmail.com>
2021-05-20 20:59:07 +0400

    Create First-Five-Multiples-of-Number.py

---
+++ First-Five-Multiples-of-Number.py
@@ -0,0 +1,7 @@
+number = int(input("Enter your number: "))
+m1 = number*1
+m2 = number*2
+m3 = number*3
+m4 = number*4
+m5 = number*5
+print("The first five multiples are",m1,",",m2,",",m3,",",m4,"and",m5,".")
