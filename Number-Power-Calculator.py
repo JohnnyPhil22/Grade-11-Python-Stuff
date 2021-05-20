98ef22ceaf468a37b04ba47afa87e70f6903763e
Jonathan Philips <johnphilips.995@gmail.com>
2021-05-20 13:42:45 +0400

    Create Number-Power-Calculator.py

---
+++ Number-Power-Calculator.py
@@ -0,0 +1,5 @@
+number = int(input("Enter your number: "))
+nrttw = number**2
+nrtth = number**3
+nrtfo = number**4
+print("The value of your number raised to two is ", nrttw, ". The value of your number raised to three is ", nrtth, ". The value of your number raised to four is ", nrtfo, ".")
