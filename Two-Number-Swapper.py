67edc0002848022c2937bf7fd329172fb865287e
Jonathan Philips <johnphilips.995@gmail.com>
2021-05-20 21:38:27 +0400

    Create Two-Number-Swapper.py

---
+++ Two-Number-Swapper.py
@@ -0,0 +1,4 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+a,b = b,a
+print("The first number after swapping is: ",a,". The second number after swapping is: ",b,".")
