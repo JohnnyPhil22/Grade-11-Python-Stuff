ee8532ae6f5701ac8e1dd7304012afd8285e524c
Jonathan Philips <johnphilips.995@gmail.com>
2021-05-24 13:15:51 +0400

    Update Area-of-Triangle-Calculator.py

--- Area-of-Triangle-Calculator.py
+++ Area-of-Triangle-Calculator.py
@@ -1,3 +1,4 @@
+# With Math Module
 import math
 
 base = int(input("Enter base of triangle: "))
@@ -10,4 +11,17 @@ side2 = int(input("Enter length of second side: "))
 side3 = int(input("Enter length of third side: "))
 s = (side1+side2+side3)/2
 ArM2 = math.sqrt(s*(s-side1)*(s-side2)*(s-side3))
+print("Area of triangle is",ArM2,".")
+
+# Without Math Module
+base = int(input("Enter base of triangle: "))
+height = int(input("Enter height of triangle: "))
+ArM1 = (base*height)/2
+print("Area of triangle is",ArM1,".")
+print("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+side1 = int(input("Enter length of first side: "))
+side2 = int(input("Enter length of second side: "))
+side3 = int(input("Enter length of third side: "))
+s = (side1+side2+side3)/2
+ArM2 = (s*(s-side1)*(s-side2)*(s-side3))**(1/2)
 print("Area of triangle is",ArM2,".")
