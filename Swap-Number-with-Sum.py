8a3b13352920f499c24c279cfe9d104dcc292a95
Jonathan Philips <johnphilips.995@gmail.com>
2021-05-24 13:25:40 +0400

    Create Swap-Number-with-Sum.py

---
+++ Swap-Number-with-Sum.py
@@ -0,0 +1,7 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+
+x,y,z = x+y,y+z,z+x
+
+print("The results are ",x,",",y,"and",z,".")
