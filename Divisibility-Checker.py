4cbbc8286e605128819e6a880c3edc709716b31d
Jonathan Philips <johnphilips.995@gmail.com>
2021-05-26 13:19:42 +0400

    Update Divisibility-Checker.py

--- Divisibility-Checker.py
+++ Divisibility-Checker.py
@@ -1,6 +1,13 @@
+# With if... else
 no1=int(input("Enter first number: "))
 no2=int(input("Enter second number: "))
 if no2%no1==0:
     print("The second number is divisible by the first number.")
 else:
     print("The second number is not divisible by the first number.")
+
+# Without if... else
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+rem = x%y
+print("The numbers are divisible: ",rem==0)
