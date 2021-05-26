e65ceee6d7e97df34081272d736b98542ecddb7c
Jonathan Philips <johnphilips.995@gmail.com>
2021-05-26 10:19:53 +0400

    Create Average-Temperature-Calc.py

---
+++ Average-Temperature-Calc.py
@@ -0,0 +1,9 @@
+sun=int(input("Enter temperature for Sunday: "))
+mon=int(input("Enter temperature for Monday: "))
+tue=int(input("Enter temperature for Tuesday: "))
+wed=int(input("Enter temperature for Wednesday: "))
+thu=int(input("Enter temperature for Thursday: "))
+fri=int(input("Enter temperature for Friday: "))
+sat=int(input("Enter temperature for Saturday: "))
+average_temp=(sun+mon+tue+wed+thu+fri+sat)/7
+print("Average temperature for this week is",average_temp,".")
