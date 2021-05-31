de93fd16533bf8f02a4ba4cc4d6c4b9f3d1a47dc
Jonathan Philips <johnphilips.995@gmail.com>
2021-05-31 17:52:09 +0400

    Update Circle-Calculator.py

--- Circle-Calculator.py
+++ Circle-Calculator.py
@@ -1,7 +1,7 @@
 ra=float(input("Enter length of radius: "))
 ch=input("Enter if you want Area/Circumference/Both for your circle (A/B/C): ")
 ar=3.14*(ra**2)
-ci=3.14*2*ra
+ci=2*3.14*ra
 if choice=="A":
     print("Area of Circle:",ar,"square units.")
 elif choice=="C":
