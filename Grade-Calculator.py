6eba0cb9b7b37f80a105f487356f03a19d93cf57
Jonathan Philips <johnphilips.995@gmail.com>
2021-05-31 17:28:59 +0400

    Update Grade-Calculator.py

--- Grade-Calculator.py
+++ Grade-Calculator.py
@@ -5,13 +5,13 @@ che=float(input("Enter marks for Chemistry: "))
 com=float(input("Enter marks for Computers: "))
 total=eng+met+phy+che+com
 percent=(total/500)*100
-if percent>=91 and percent<=100:
+if 91<=percent<=100:
     print("Your percentage is ", percent, ". Your grade is A. You've passed!")
-elif percent>=81 and percent<=90:
+elif 81<=percent<=90:
     print("Your percentage is ", percent, ". Your grade is B. You've passed!")
-elif percent>=71 and percent<=80:
+elif 71<=percent<=80:
     print("Your percentage is ", percent, ". Your grade is C. You've passed!")
-elif percent>=61 and percent<=70:
+elif 61<=percent<=70:
     print("Your percentage is ", percent, ". Your grade is D. You've passed!")
 elif percent<=60:
     print("Your percentage is ", percent, ". You've failed!")
