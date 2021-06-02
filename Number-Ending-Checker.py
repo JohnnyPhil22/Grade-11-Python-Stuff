148b67c22ff2586db753bf5028e2495ca752388c
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-02 20:15:30 +0400

    Update Number-Ending-Checker.py

--- Number-Ending-Checker.py
+++ Number-Ending-Checker.py
@@ -1,8 +1,7 @@
 no = int(input("Enter a number: "))
 if no%10==4:
     print("Ends with 4.")
+elif no%10==8:
+    print("Ends with 8.")
 else:
-    if no%10==8:
-        print("Ends with 8.")
-    else:
-        print("Ends with neither.")
+    print("Ends with neither.")
