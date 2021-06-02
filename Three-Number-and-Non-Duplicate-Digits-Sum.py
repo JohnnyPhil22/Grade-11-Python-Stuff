384bc86edd51fbabbfc5de42c4db0dda0aacd354
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-02 10:11:50 +0400

    Update Three-Number-and-Non-Duplicate-Digits-Sum.py

--- Three-Number-and-Non-Duplicate-Digits-Sum.py
+++ Three-Number-and-Non-Duplicate-Digits-Sum.py
@@ -4,11 +4,11 @@ n3 = int(input("Enter third number: "))
 sum1 = n1+n2+n3
 print("Sum of all numbers is:",sum1,".")
 if n1==n2:
-    sum2 = n1+n3
+    sum2 = n3
     print("Sum of all non-repeating numbers is:",sum2,".")
 elif n1==n3:
-    sum2 = n1+n2
+    sum2 = n2
     print("Sum of all non-repeating numbers is:",sum2,".")
 elif n2==n3:
-    sum2 = n2+n1
+    sum2 = n1
     print("Sum of all non-repeating numbers is:",sum2,".")
