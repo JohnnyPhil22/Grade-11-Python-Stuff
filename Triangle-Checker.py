e773fa877bcf2e7db9a8b81d9d4a78c2c1d391f4
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-06 11:02:37 +0400

    Update Triangle-Checker.py

--- Triangle-Checker.py
+++ Triangle-Checker.py
@@ -2,11 +2,7 @@ a = float(input("Enter length of first side: "))
 b = float(input("Enter length of second side: "))
 c = float(input("Enter length of third side: "))
 
-if c>(a+b):
-    print("These are sides of a triangle.")
-elif a>(b+c):
-    print("These are sides of a triangle.")
-elif b>(c+a):
+if c>(a+b) or a>(b+c) or b>(c+a):
     print("These are sides of a triangle.")
 else:
     print("These are not sides of a triangle.")
