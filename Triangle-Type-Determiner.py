3475ac2c0506d790bea5d65a1b4c6e29f8a8b496
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-06 11:12:47 +0400

    Update Triangle-Type-Determiner.py

--- Triangle-Type-Determiner.py
+++ Triangle-Type-Determiner.py
@@ -1,7 +1,9 @@
 a = float(input("Enter length of first side: "))
 b = float(input("Enter length of second side: "))
 c = float(input("Enter length of third side: "))
-if a==b==c:
+if a<=0 or b<=0 or c<=0:
+    print("Please enter a value greater than 0.")
+elif a==b==c:
     print("The triangle is equilateral.")
 else:
     if a==b or b==c or c==a:
