576a056d2704591a58d4145547d0b59e0be78df0
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-13 11:03:05 +0400

    Update Maximum-and-Minimum-Number.py

--- Maximum-and-Minimum-Number.py
+++ Maximum-and-Minimum-Number.py
@@ -1,7 +1,7 @@
-import math
-maxno=-math.inf
-minno=math.inf
-for i in range(1,11):
+n=int(input("Enter number < 0: "))
+maxno=n
+minno=n
+for i in range(1,10):
     n=int(input("Enter number < 0: "))
     if n>maxno:
         maxno=n
