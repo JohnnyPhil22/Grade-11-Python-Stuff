a46506ebde27917188895cd1a39ae3deda6fbe69
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-16 13:21:26 +0400

    Update Pattern-Printer.py

--- Pattern-Printer.py
+++ Pattern-Printer.py
@@ -20,34 +20,6 @@ for i in range(1,6):
         else:
             print("$",end='')
     print('')
-
-'''
-1
-10
-101
-1010
-10101
-'''
-for i in range(1,6):
-    for j in range(1,i+1):
-        if j%2==0:
-            print("0",end='')
-        else:
-            print("1",end='')
-    print('')
-'''
-1
-101
-10101
-1010101
-'''
-for i in range(1,8,2):
-    for j in range(1,i+1):
-        if j%2==0:
-            print("0",end='')
-        else:
-            print("1",end='')
-    print('')
 '''
 A
 AB
@@ -59,26 +31,3 @@ for i in range(65,70):
     for j in range(65,i+1):
         print(chr(j),end='')
     print('')
-'''
-55555
-4444
-333
-22
-1
-'''
-
-'''
-*****
-****
-***
-**
-*
-'''
-
-'''
-    *
-   * *
-  * * *
- * * * *
-* * * * *
-'''
