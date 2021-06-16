cc9c992058a7ed847f85001c44745ac9cbd63e60
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-16 13:18:47 +0400

    Update Number-Ladders.py

--- Number-Ladders.py
+++ Number-Ladders.py
@@ -9,7 +9,6 @@ for i in range(1,6):
     for j in range(1,i+1):
         print(j,end='')
     print()
-
 '''
 1
 22
@@ -26,3 +25,37 @@ for i in range(1,6):
 for i in range(1,6):
     print(str(i)*i,end='')
     print()
+'''
+1
+10
+101
+1010
+10101
+'''
+for i in range(1,6):
+    for j in range(1,i+1):
+        if j%2==0:
+            print("0",end='')
+        else:
+            print("1",end='')
+    print('')
+'''
+1
+101
+10101
+1010101
+'''
+for i in range(1,8,2):
+    for j in range(1,i+1):
+        if j%2==0:
+            print("0",end='')
+        else:
+            print("1",end='')
+    print('')
+'''
+55555
+4444
+333
+22
+1
+'''
