6da36dfba20df35392d2fb69e255b06a869d1121
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-16 10:32:18 +0400

    Create Pattern-Printer.py

---
+++ Pattern-Printer.py
@@ -0,0 +1,59 @@
+'''
+$
+@@
+$$$
+@@@@
+$$$$$
+'''
+# Simple For Loop
+for i in range(1,6):
+    if i%2==0:
+        print("@"*i,end='')
+    else:
+        print("$"*i,end='')
+    print('')
+# Nested For Loop
+for i in range(1,6):
+    for j in range(1,i+1):
+        if i%2==0:
+            print("@",end='')
+        else:
+            print("$",end='')
+    print('')
+
+'''
+1
+10
+101
+1010
+10101
+'''
+for i in range(1,6):
+    for j in range(1,i+1):
+        if j%2==0:
+            print("0",end='')
+        else:
+            print("1",end='')
+    print('')
+'''
+1
+101
+10101
+1010101
+'''
+
+'''
+A
+AB
+ABC
+ABCD
+ABCDE
+'''
+
+'''
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+'''
