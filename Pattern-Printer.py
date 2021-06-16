a43e69a2905358d09f95115815b3042b220c3267
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-16 13:12:32 +0400

    Update Pattern-Printer.py

--- Pattern-Printer.py
+++ Pattern-Printer.py
@@ -41,13 +41,38 @@ for i in range(1,6):
 10101
 1010101
 '''
-
+for i in range(1,8,2):
+    for j in range(1,i+1):
+        if j%2==0:
+            print("0",end='')
+        else:
+            print("1",end='')
+    print('')
 '''
 A
 AB
 ABC
 ABCD
 ABCDE
+'''
+for i in range(65,70):
+    for j in range(65,i+1):
+        print(chr(j),end='')
+    print('')
+'''
+55555
+4444
+333
+22
+1
+'''
+
+'''
+*****
+****
+***
+**
+*
 '''
 
 '''
