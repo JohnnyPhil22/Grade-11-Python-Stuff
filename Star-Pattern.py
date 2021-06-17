5247989eb4929e3a2aefaac0564d97eb6fc15809
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-17 14:16:59 +0400

    Update Star-Pattern.py

--- Star-Pattern.py
+++ Star-Pattern.py
@@ -25,3 +25,24 @@ for i in range(5):
  * * * *
 * * * * *
 '''
+for i in range(1,6):
+    for sp in range(6,i,-1):
+        print(' ',end='')
+    for j in range(0,i):
+        print('* ',end='')
+    print('')
+'''
+   *
+  *** 
+ *****
+*******
+'''
+for i in range(1,8,2):
+    for sp in range(6,i,-1):
+        print(' ',end='')
+    for j in range(0,i):
+        print('* ',end='')
+    print('')
+'''
+
+'''
