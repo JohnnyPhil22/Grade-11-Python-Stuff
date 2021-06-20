75d38f21e427e02eb9b97e7eb2c296acef0dd42b
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-20 20:55:11 +0400

    Update Star-Pattern.py

--- Star-Pattern.py
+++ Star-Pattern.py
@@ -79,3 +79,26 @@ for i in range(4,0,-1):
     for j in range(i,0,-1):
         print('* ',end='')
     print('')
+'''
+     *
+    * *
+   *   *
+  *     *
+ *       *
+***********
+'''
+k=0
+for i in range(1,7):
+    for j in range(i,6):
+        print(' ',end='')
+    while k!=(2*i-1):
+        if k==0 or k==2*i-2:
+            print('*', end='')
+        else:
+            print(' ', end='')
+        k+=1
+    k=0
+    print('')
+for i in range(0, 11):
+    print('*', end='')
+print('')
