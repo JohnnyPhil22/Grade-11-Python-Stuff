0d7c0cb8809c90ee255850ae3f3a2df14698e7b3
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-20 22:06:54 +0400

    Update Pattern-Printer.py

--- Pattern-Printer.py
+++ Pattern-Printer.py
@@ -31,3 +31,23 @@ for i in range(65,70):
     for j in range(65,i+1):
         print(chr(j),end='')
     print('')
+'''
+   A
+  BAB
+ CBABC
+DCBABCD
+'''
+for i in range(0,4):
+    for sp in range(6,i,-1):
+        print(' ',end='')
+    for d in range(65+i,64,-1):
+        print(chr(d),end='')
+    for a in range(66,66+i,1):
+        print(chr(a),end='')
+    print('')
+'''
+   A
+  ABA
+ ABCBA
+ABCDCBA
+'''
