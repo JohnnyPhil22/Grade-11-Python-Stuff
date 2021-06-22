18d5d4cbf6ae122781522d219737f4adb1386a55
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-22 09:39:42 +0400

    Update Prime-Number-between-Two-Limits.py

--- Prime-Number-between-Two-Limits.py
+++ Prime-Number-between-Two-Limits.py
@@ -1,9 +1,8 @@
-ll = int(input("Enter lower limit: "))
-ul = int(input("Enter upper limit: "))
-for i in range(ll, ul+1):
+ll=int(input("Enter lower limit: "))
+ul=int(input("Enter upper limit: "))
+for i in range(ll,ul+1):
     if i==1:
         continue
-    f=1
     for j in range(2,i//2+1):
         if i%j==0:
             break
