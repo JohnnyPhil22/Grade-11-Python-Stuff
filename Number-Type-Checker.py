731fc59b49b0fc0005e216f190e6e354a78b405a
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-23 11:41:35 +0400

    Update Number-Type-Checker.py

--- Number-Type-Checker.py
+++ Number-Type-Checker.py
@@ -1,3 +1,14 @@
 # Palindrome checker
 
 # Armstrong checker
+n=int(input("Enter number: "))
+s,temp=0,n
+while temp>0:
+    d=temp%10
+    s+=d**3
+    temp//=10
+print(s)
+if s==n:
+    print("It is an Armstrong number.")
+else:
+    print("It is not an Armstrong number.")
