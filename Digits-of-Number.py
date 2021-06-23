c5f04050b130325d3f7b1723fabae50ff1f88a35
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-23 11:34:27 +0400

    Update Digits-of-Number.py

--- Digits-of-Number.py
+++ Digits-of-Number.py
@@ -26,14 +26,6 @@ while n>0:
     n//=10
     s+=d
 print("Sum of digits:",s)
-# Reverse of number
-n=int(input("Enter number: "))
-r=0
-while n>0:
-    d=n%10
-    r=r*10+d
-    n//=10
-print("Reversed number:",r)
 # Largest digit
 
 # Number of odd digits
