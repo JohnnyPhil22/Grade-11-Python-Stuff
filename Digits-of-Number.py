af82ccca3ca41a46de7223aa2f87b3462b3b34ed
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-23 10:10:24 +0400

    Update Digits-of-Number.py

--- Digits-of-Number.py
+++ Digits-of-Number.py
@@ -1,5 +1,13 @@
+# All digits
 n=int(input("Enter number: "))
 while n>0:
     d=n%10
     print(d)
     n//=10
+# Even digits
+n=int(input("Enter number: "))
+while n>0:
+    d=n%10
+    n//=10
+    if d%2==0:
+        print(d)
