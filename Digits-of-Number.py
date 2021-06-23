7d9b26a8498a1a0b89c45dfad52cd1911e85d9ea
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-23 19:39:56 +0400

    Update Digits-of-Number.py

--- Digits-of-Number.py
+++ Digits-of-Number.py
@@ -36,3 +36,11 @@ while n>0:
     n//=10
 print("Largest digit:",ld)
 # Number of odd digits
+n=int(input("Enter number: "))
+oc=0
+while n>0:
+    d=n%10
+    if d%2!=0:
+        oc+=1
+    n//=10
+print("Number of odd digits:",oc)
