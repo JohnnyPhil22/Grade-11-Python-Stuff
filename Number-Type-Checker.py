bb5a1872fe7f1d2c356e7fbfc87bc4b0b8eaa28e
Jonathan Philips <johnphilips.995@gmail.com>
2021-06-23 16:29:10 +0400

    Update Number-Type-Checker.py

--- Number-Type-Checker.py
+++ Number-Type-Checker.py
@@ -1,5 +1,15 @@
 # Palindrome checker
-
+n=int(input("Enter number: "))
+r,dn=0,n
+while n>0:
+    d=n%10
+    r=r*10+d
+    n//=10
+print(r)
+if r==dn:
+    print("Number is a palindrome.")
+else:
+    print("Number is not a palindrome.")
 # Armstrong checker
 n=int(input("Enter number: "))
 s,dn=0,n
