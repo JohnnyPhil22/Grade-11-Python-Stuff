f099d6d34cd7c188ba1c373c6f3ffcc976925d15
Jonathan Philips <johnphilips.995@gmail.com>
2021-07-08 12:29:02 +0400

    Update Practice-Questions.py

--- Practice-Questions.py
+++ Practice-Questions.py
@@ -181,3 +181,14 @@ for i in range(1,n1+1):
     if n1%i==0 and n2%i==0:
         hcf=i
 print("Highest Common Factor:",hcf)
+# FizzBuzz
+for i in range(1,51):
+    if i%5==0 and i%3==0:
+        print("FizzBuzz")
+    elif i%3==0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+# Vowel or consonant letter
