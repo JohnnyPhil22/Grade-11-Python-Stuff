4516fa5a15359a89315c1d9fe6955448ad8cfacc
Jonathan Philips <johnphilips.995@gmail.com>
2021-08-06 10:24:25 +0000

    Update Summer-Assignment.py

--- Summer-Assignment.py
+++ Summer-Assignment.py
@@ -8,6 +8,8 @@ m=int(input("Enter month (1 to 12): "))
 y=int(input("Enter year: "))
 if y%4==0 and m==2 and d==29:
     print("It is a valid date.")
+elif y%4==0 and 0<m<=12 and 1<=d<=31:
+    print("It is a valid date.")
 elif (m==1 or m==3 or m==5 or m==7 or m==9 or m==11) and d<=31:
     print("It is a valid date.")
 elif (m==4 or m==6 or m==8 or m==10 or m==12) and d<=30:
