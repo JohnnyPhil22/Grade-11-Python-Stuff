221076f27ef25b3fdfe53a090c73f3135b86b16b
Jonathan Philips <johnphilips.995@gmail.com>
2021-08-31 20:37:40 +0400

    Update String-Questions.py

--- String-Questions.py
+++ String-Questions.py
@@ -1,5 +1,5 @@
 # Input a string and count number of times 'a', 'A' appear in the sentence.
-s=input("Enter anything: ")
+s=input("Enter a string: ")
 count=0
 for i in range(0,len(s)):
     if s[i]=='a' or s[i]=='A':
@@ -7,7 +7,7 @@ for i in range(0,len(s)):
 print('Number of times \'a\' or \'A\' appear:',count)
 
 # Input string and count number of vowels.
-s=input("Enter anything: ")
+s=input("Enter a string: ")
 count=0
 for i in range(0,len(s)):
     if s[i]=='a' or s[i]=='A' or s[i]=='e' or s[i]=='E' or s[i]=='i' or s[i]=='I' or s[i]=='o' or s[i]=='O' or s[i]=='u' or s[i]=='U':
@@ -15,7 +15,7 @@ for i in range(0,len(s)):
 print('Number of times vowels appear:',count)
 
 # Input tring and count number of consonants.
-s=input("Enter anything: ")
+s=input("Enter a string: ")
 count=0
 for i in range(0,len(s)):
     if s[i]!='a' or s[i]!='A' or s[i]!='e' or s[i]!='E' or s[i]!='i' or s[i]!='I' or s[i]!='o' or s[i]!='O' or s[i]!='u' or s[i]!='U':
@@ -23,14 +23,14 @@ for i in range(0,len(s)):
 print('Number of times consonants appear:',count)
 
 # Input string and count number of characters.
-s=input("Enter anything: ")
+s=input("Enter a string: ")
 count=0
 for i in range(0,len(s)):
     count+=1
 print('Number of characters appear:',count)
 
 # Input string and count number of lowercase characters.
-s=input("Enter anything: ")
+s=input("Enter a string: ")
 count=0
 for i in range(0,len(s)):
     if s[i]=='a' or s[i]=='b' or s[i]=='c' or s[i]=='d' or s[i]=='e' or s[i]=='f' or s[i]=='g' or s[i]=='h' or s[i]=='i' or s[i]=='j' or s[i]=='k' or s[i]=='l' or s[i]=='m' or s[i]=='n' or s[i]=='o' or s[i]=='p' or s[i]=='q' or s[i]=='r' or s[i]=='s' or s[i]=='t' or s[i]=='u' or s[i]=='v' or s[i]=='w' or s[i]=='x' or s[i]=='y' or s[i]=='z':
@@ -38,7 +38,7 @@ for i in range(0,len(s)):
 print('Number of characters appear:',count)
 
 # Input string and count number of uppercase characters.
-s=input("Enter anything: ")
+s=input("Enter a string: ")
 count=0
 for i in range(0,len(s)):
     if s[i]=='A' or s[i]=='B' or s[i]=='C' or s[i]=='D' or s[i]=='E' or s[i]=='F' or s[i]=='G' or s[i]=='H' or s[i]=='I' or s[i]=='J' or s[i]=='K' or s[i]=='L' or s[i]=='M' or s[i]=='N' or s[i]=='O' or s[i]=='P' or s[i]=='Q' or s[i]=='R' or s[i]=='S' or s[i]=='T' or s[i]=='U' or s[i]=='V' or s[i]=='W' or s[i]=='X' or s[i]=='Y' or s[i]=='Z':
