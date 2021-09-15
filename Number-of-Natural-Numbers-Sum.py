f79ad49f5b998561666b70e080929cb977d22408
Jonathan Philips <johnphilips.995@gmail.com>
2021-09-15 14:06:35 +0400

    Update Number-of-Natural-Numbers-Sum.py

--- Number-of-Natural-Numbers-Sum.py
+++ Number-of-Natural-Numbers-Sum.py
@@ -1,5 +1,5 @@
-n = int(input("Enter number of natural numbers for sum: "))
-sum = 0
+n=int(input("Enter number of natural numbers for sum: "))
+s=0
 for i in range(1, n+1):
-    sum = sum+i
-print("Sum:",sum)
+    s+=i
+print("Sum:",s)
