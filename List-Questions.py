3cf748a85fd7a11a7ef98d0a14a5411627553fc0
Jonathan Philips <johnphilips.995@gmail.com>
2021-10-05 15:56:34 +0400

    Update List-Questions.py

--- List-Questions.py
+++ List-Questions.py
@@ -1,4 +1,19 @@
 # Write a program to create nested list with 10 elements where 4th element is nested list with 4 elements. Access all elements with indexing.
+list1=[0,0,7,['J','P',2,2],9,9,5,'l','s','t']
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[3][0])
+print(list1[3][1])
+print(list1[3][2])
+print(list1[3][3])
+print(list1[4])
+print(list1[5])
+print(list1[6])
+print(list1[7])
+print(list1[8])
+print(list1[9])
 
 # Create two lists and concatenate them.
 
