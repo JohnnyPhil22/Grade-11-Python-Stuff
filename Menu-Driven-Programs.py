ce4231fa188b394cd6aea6b3e12423e1f8929317
Jonathan Philips <johnphilips.995@gmail.com>
2021-10-17 18:35:20 +0400

    Update Menu-Driven-Programs.py

--- Menu-Driven-Programs.py
+++ Menu-Driven-Programs.py
@@ -1,3 +1,4 @@
+# Menu Driven Program for area and circumference of circle & area and perimeter of rectangle.
 opt='y'
 while opt in 'yY':
     print("1. Area of circle")
@@ -21,4 +22,50 @@ while opt in 'yY':
         print("Perimeter of rectangle:",(2*(l+b)))
     else:
         print("Select from options 1 through 4.")
+    opt=input("Press y to continue: ")
+
+# Write a menu driven program to perform various list operations.
+opt='y'
+l=eval(input('Enter elements for list (Enter with [ and ]): '))
+while opt in 'yY':
+    print('1. Display list')
+    print('2. Append an element')
+    print('3. Insert an element')
+    print('4. Append list to given list')
+    print('5. Modify existing element')
+    print('6. Delete existing element from position')
+    print('7. Delete existing element with given value')
+    print('8. Sort list in ascending order')
+    print('9. Sort list in descending order')
+    ch=int(input('Enter choice: '))
+    if ch==1:
+        print('List:',l)
+    elif ch==2:
+        e=eval(input('Enter element: '))
+        l.append(e)
+    elif ch==3:
+        e=eval(input('Enter element: '))
+        p=int(input('Enter position: '))
+        l.insert(p,e)
+    elif ch==4:
+        nl=eval(input('Enter list: '))
+        l.extend(nl)
+    elif ch==5:
+        i=int(input('Enter index number: '))
+        e=eval(input('Enter element to replace: '))
+        del l[i]
+        l.insert(i,e)
+    elif ch==6:
+        i=int(input('Enter index number: '))
+        del l[i]
+    elif ch==7:
+        e=eval(input('Enter element to remove: '))
+        l.remove(e)
+    elif ch==8:
+        l.sort()
+    elif ch==9:
+        l.sort()
+        l.reverse()
+    else:
+        print("Select from options 1 through 9.")
     opt=input("Press y to continue: ")
