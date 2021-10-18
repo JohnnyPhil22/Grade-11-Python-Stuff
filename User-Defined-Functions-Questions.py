b05cab5d92e654736760a7fb12eaf58be346a87b
Jonathan Philips <johnphilips.995@gmail.com>
2021-10-18 11:05:56 +0400

    Update User-Defined-Functions-Questions.py

--- User-Defined-Functions-Questions.py
+++ User-Defined-Functions-Questions.py
@@ -1,4 +1,4 @@
-# Function to increment elements of list passed as arguments
+# Increment elements of list passed as arguments
 def increment(list2):
     for i in range(0,len(list2)):
         list2[i]+=5
@@ -6,3 +6,18 @@ list1=[10,20,30,40,50]
 print('Original list:',list1)
 increment(list1)
 print('New list:',list1)
+
+# Average of marks
+def avgmarks(l,n):
+    s=0
+    for m in l:
+        s+=m
+    a=s/n
+    return a
+l=[]
+n=int(input('Enter number of students: '))
+for i in range(0,n):
+    m=int(input('Enter marks of student: '))
+    l.append(m)
+a=avgmarks(l,n)
+print('Average of marks:',a)
