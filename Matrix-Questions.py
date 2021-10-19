0932677b19aaa93da154899bf392d33a2ca14628
Jonathan Philips <johnphilips.995@gmail.com>
2021-10-19 07:39:44 +0400

    Update Matrix-Questions.py

--- Matrix-Questions.py
+++ Matrix-Questions.py
@@ -5,13 +5,14 @@ c=int(input("Enter number of columns: "))
 for i in range(r):
     row=[]
     for j in range(c):
-        e=int(input('Element at '+str(i)+','+str(j)+': '))
+        e=int(input("Element at "+str(i)+","+str(j)+": "))
         row.append(e) 
     l.append(row)
-print("List created is:",l)
+print("List created:",l)
 print("List=[")
 for i in range(r):
-    print("\t",end="")
+    print("\t[ ",end=" ")
     for j in range(c):
-        print(l[i][j],end="")
+        print(l[i][j],end=" ")
     print("]")
+print("\t]")
