df2473923c49ef3d9dc48f96ef6358bacb2e77cb
Jonathan Philips <johnphilips.995@gmail.com>
2021-10-23 13:04:06 +0400

    Update Matrix-Questions.py

--- Matrix-Questions.py
+++ Matrix-Questions.py
@@ -40,3 +40,42 @@ s=0
 for k in range(r):
     s+=l[k][k]
 print('Sum of elements in diagonals:',s)
+
+# Create 2 matrices & find sum
+l1=[]
+r=int(input("Enter number of rows: "))
+c=int(input("Enter number of columns: "))
+print('---------- LIST 1 ----------')
+for i in range(r):
+    row1=[]
+    for j in range(c):
+        e=int(input("Element at "+str(i)+","+str(j)+": "))
+        row1.append(e) 
+    l1.append(row1)
+print("2D List created:",l1)
+print("3D List: [")
+for i in range(r):
+    print("\t [ ",end="")
+    for j in range(c):
+        print(l1[i][j],end=" ")
+    print("]")
+print("\t ]")
+print('---------- LIST 2 ----------')
+l2=[]
+for i in range(r):
+    row2=[]
+    for j in range(c):
+        e=int(input("Element at "+str(i)+","+str(j)+": "))
+        row2.append(e) 
+    l2.append(row2)
+print("2D List created:",l2)
+print("3D List: [")
+for i in range(r):
+    print("\t [ ",end="")
+    for j in range(c):
+        print(l2[i][j],end=" ")
+    print("]")
+print("\t ]")
+s=[[l1[i][j]+l2[i][j] for j in range(len(l1[0]))] for i in range(len(l1))]
+for k in s:
+   print(s)
