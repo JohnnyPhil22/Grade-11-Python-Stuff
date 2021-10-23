ab95e5cb0de1d3892e69b43500b0726f6e566fff
Jonathan Philips <johnphilips.995@gmail.com>
2021-10-23 11:15:57 +0400

    Update Matrix-Questions.py

--- Matrix-Questions.py
+++ Matrix-Questions.py
@@ -17,7 +17,7 @@ for i in range(r):
     print("]")
 print("\t]")
 
-# Create matrix of 2 by 3 2D list and find sum of elements of first row
+# Create matrix of 2 by 3 2D list & find sum of elements of first row and diagonals
 l=[]
 r=int(input("Enter number of rows: "))
 c=int(input("Enter number of columns: "))
@@ -28,11 +28,13 @@ for i in range(r):
         row.append(e) 
     l.append(row)
 print("2D List created:",l)
-print("3D List=[")
+print("3D List: [")
 for i in range(r):
-    print("\t[ ",end=" ")
+    print("\t [ ",end=" ")
     for j in range(c):
         print(l[i][j],end=" ")
     print("]")
-print("\t]")
+print("\t ]")
 print('Sum of elements in first row:',sum(l[0]))
+s=(l[0][0])+(l[1][2])
+print('Sum of elements in diagonals:',s)
