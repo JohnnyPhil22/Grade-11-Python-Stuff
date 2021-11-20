f8ea3e827a870095ab9d76dbf8a4756c3fe6a4cb
Jonathan Philips <johnphilips.995@gmail.com>
2021-11-20 21:42:42 +0400

    Update Number-Type-Checker.py

--- Number-Type-Checker.py
+++ Number-Type-Checker.py
@@ -22,3 +22,25 @@ if s==n:
     print("It is an Armstrong number.")
 else:
     print("It is not an Armstrong number.")
+# Perfect number checker
+## While loop
+n=int(input("Enter number: "))
+s,i=0,1
+while i<n:
+    if(n%i==0):
+        s+=i
+    i+=1
+if s==n:
+    print("It's a perfect number.")
+else:
+    print("It isn't a perfect number.")       
+## For loop
+n=int(input("Enter number: "))
+s=0
+for i in range(1,n):
+    if(n%i==0):
+        s=s+i
+if s==n:
+    print("It's a perfect number.")
+else:
+    print("It isn't a perfect number.")
